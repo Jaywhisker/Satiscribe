9e88845773d3a18b43c10312e0193df32e73a407
Jaywhisker <51687528+Jaywhisker@users.noreply.github.com>
2023-07-22 01:28:45 +0800

    added colour

---
+++ SatiscribeFrontEnd/pages/WX/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import style from '../../styles/Colourtest.module.css'
+
+function colourtest() {
+    return (
+        <div className={style.container}>
+            <p className={style.text1}>Final_Light_Purple</p>
+            <p className={style.text2}>Final_Light_Purple_75</p>
+            <p className={style.text3}>Final_Light_Purple_50</p>
+            <p className={style.text4}>Final_Light_Purple_25</p>
+            <p className={style.text5}>Final_Dark_Purple</p>
+            <p className={style.text6}>Final_Dark_Purple_80</p>
+            <p className={style.text7}>Final_Gray</p>
+            <p className={style.text8}>Final_Gray_80</p>
+            <p className={style.text9}>Final_White</p>
+            <p className={style.text10}>Final_White_50</p>
+            <p className={style.text11}>Final_Component_Dark_Grey</p>
+            <p className={style.text12}>Final_Component_Lighter_Grey</p>
+            <p className={style.text13}>Final_Red</p>
+            <p className={style.text14}>Final_Yellow</p>
+            <p className={style.text15}>Final_Profile_Purple</p>
+            <p className={style.text16}>Final_Profile_Red</p>
+            <p className={style.text17}>Final_Profile_Grey</p>
+            <p className={style.text18}>Final_Profile_LightGrey</p>
+            <p className={style.text19}>Final_Profile_Blue</p>
+            <p className={style.text20}>Final_Profile_Orange</p>
+            <p className={style.text21}>Final_Profile_Green</p>
+            <p className={style.text22}>Final_Profile_Pink</p>
+            <p className={style.text23}>Final_Profile_White</p>
+            <p className={style.text24}>Final_Calendar_Green</p>
+            <p className={style.text25}>Final_Calendar_Purple</p>
+            <p className={style.text26}>Final_Calendar_Skin</p>
+            <p className={style.text27}>Final_Calendar_Pink</p>
+
+
+        </div>
+    )
+}
+
+export default colourtest
--- SatiscribeFrontEnd/pages/_app.js
+++ SatiscribeFrontEnd/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import '../styles/colours.css';
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />;
