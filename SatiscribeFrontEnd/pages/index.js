b93d45ccd1badefd7ac168064ea89b4f18c3fbf6
Shockbob17 <94674990+Shockbob17@users.noreply.github.com>
2023-07-22 00:30:19 +0800

    some stuff

--- SatiscribeFrontEnd/pages/index.js
+++ SatiscribeFrontEnd/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../styles/Logos.module.css'
+import logos from '../styles/Logos.module.css'
 import flexi from '../styles/Flexible.module.css'
 
 
@@ -7,83 +7,83 @@ export default function index() {
     return (
         <div>
             <div>
-                <h1>Haro this is the display of the stylesheet</h1>
+                <h1>Haro this is the display of the logosheet</h1>
             </div>
 
             <h2>Flexible module css: display layouts</h2>
 
             <p>This uses the flexrow found in Flexible.module.css, the gap can be controlled from global.css</p>
             <div className={flexi.flexrow}>
-                <div className={styles.smallCancel}></div>
-                <div className={styles.smallCancel}></div>
-                <div className={styles.bigCancel}></div>
+                <div className={logos.small} style={{ backgroundImage=`url()` }}></div>
+                <div className={logos.smallCancel}></div>
+                <div className={logos.bigCancel}></div>
             </div>
 
             <p>This uses the flexcolumn found in Flexible.module.css, the gap can be controlled from global.css</p>
             <div className={flexi.flexcolumn}>
-                <div className={styles.smallCancel}></div>
-                <div className={styles.smallCancel}></div>
-                <div className={styles.bigCancel}></div>
+                <div className={logos.smallCancel}></div>
+                <div className={logos.smallCancel}></div>
+                <div className={logos.bigCancel}></div>
             </div>
 
             <h2>Icons</h2>
             <p>Lmao they are so pixalated</p>
 
             <div className={flexi.flexrow}>
-                <div className={styles.smallCancel}></div>
-                <div className={styles.smallBack}></div>
-                <div className={styles.smallAlignleft}></div>
-                <div className={styles.smallCaution}></div>
-                <div className={styles.smallCheck}></div>
-                <div className={styles.smallDropdown}></div>
-                <div className={styles.smallEdit}></div>
-                <div className={styles.smallFile}></div>
-                <div className={styles.smallFilter}></div>
-                <div className={styles.smallHamburger}></div>
-                <div className={styles.smallHome}></div>
-                <div className={styles.smallInfo}></div>
-                <div className={styles.smallLink}></div>
-                <div className={styles.smallLogout}></div>
-                <div className={styles.smallMaps}></div>
-                <div className={styles.smallPlus}></div>
-                <div className={styles.smallProfile}></div>
-                <div className={styles.smallSave}></div>
-                <div className={styles.smallSearch}></div>
-                <div className={styles.smallSort}></div>
-                <div className={styles.smallStart}></div>
-                <div className={styles.smallTrash}></div>
-                <div className={styles.smallComment}></div>
-                <div className={styles.smallEditpage}></div>
-                <div className={styles.smallReplace}></div>
+                <div className={logos.smallCancel}></div>
+                <div className={logos.smallBack}></div>
+                <div className={logos.smallAlignleft}></div>
+                <div className={logos.smallCaution}></div>
+                <div className={logos.smallCheck}></div>
+                <div className={logos.smallDropdown}></div>
+                <div className={logos.smallEdit}></div>
+                <div className={logos.smallFile}></div>
+                <div className={logos.smallFilter}></div>
+                <div className={logos.smallHamburger}></div>
+                <div className={logos.smallHome}></div>
+                <div className={logos.smallInfo}></div>
+                <div className={logos.smallLink}></div>
+                <div className={logos.smallLogout}></div>
+                <div className={logos.smallMaps}></div>
+                <div className={logos.smallPlus}></div>
+                <div className={logos.smallProfile}></div>
+                <div className={logos.smallSave}></div>
+                <div className={logos.smallSearch}></div>
+                <div className={logos.smallSort}></div>
+                <div className={logos.smallStart}></div>
+                <div className={logos.smallTrash}></div>
+                <div className={logos.smallComment}></div>
+                <div className={logos.smallEditpage}></div>
+                <div className={logos.smallReplace}></div>
             </div>
             <div className={flexi.flexrow}>
-                <div className={styles.bigCancel}></div>
-                <div className={styles.bigBack}></div>
-                <div className={styles.bigAlignleft}></div>
-                <div className={styles.bigCaution}></div>
-                <div className={styles.bigCheck}></div>
-                <div className={styles.bigDropdown}></div>
-                <div className={styles.bigEdit}></div>
-                <div className={styles.bigFile}></div>
-                <div className={styles.bigFilter}></div>
-                <div className={styles.bigHamburger}></div>
-                <div className={styles.bigHome}></div>
-                <div className={styles.bigInfo}></div>
-                <div className={styles.bigLink}></div>
+                <div className={logos.bigCancel}></div>
+                <div className={logos.bigBack}></div>
+                <div className={logos.bigAlignleft}></div>
+                <div className={logos.bigCaution}></div>
+                <div className={logos.bigCheck}></div>
+                <div className={logos.bigDropdown}></div>
+                <div className={logos.bigEdit}></div>
+                <div className={logos.bigFile}></div>
+                <div className={logos.bigFilter}></div>
+                <div className={logos.bigHamburger}></div>
+                <div className={logos.bigHome}></div>
+                <div className={logos.bigInfo}></div>
+                <div className={logos.bigLink}></div>
             </div>
             <div className={flexi.flexrow}>
-                <div className={styles.bigLogout}></div>
-                <div className={styles.bigMaps}></div>
-                <div className={styles.bigPlus}></div>
-                <div className={styles.bigProfile}></div>
-                <div className={styles.bigSave}></div>
-                <div className={styles.bigSearch}></div>
-                <div className={styles.bigSort}></div>
-                <div className={styles.bigStart}></div>
-                <div className={styles.bigTrash}></div>
-                <div className={styles.bigComment}></div>
-                <div className={styles.bigEditpage}></div>
-                <div className={styles.bigReplace}></div>
+                <div className={logos.bigLogout}></div>
+                <div className={logos.bigMaps}></div>
+                <div className={logos.bigPlus}></div>
+                <div className={logos.bigProfile}></div>
+                <div className={logos.bigSave}></div>
+                <div className={logos.bigSearch}></div>
+                <div className={logos.bigSort}></div>
+                <div className={logos.bigStart}></div>
+                <div className={logos.bigTrash}></div>
+                <div className={logos.bigComment}></div>
+                <div className={logos.bigEditpage}></div>
+                <div className={logos.bigReplace}></div>
             </div>
         </div>
     )
