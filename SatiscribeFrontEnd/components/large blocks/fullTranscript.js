d4dbc3f36dec7f5070b087ba03aa2263f26fc8bb
Jaywhisker <51687528+Jaywhisker@users.noreply.github.com>
2023-07-24 02:23:10 +0800

    added large blocks

---
+++ SatiscribeFrontEnd/components/large blocks/fullTranscript.js
@@ -0,0 +1,109 @@
+import React from 'react'
+import style from '../../styles/Colourtest.module.css'
+import flexi from '../../styles/Flexible.module.css'
+import logos from '../../styles/Logos.module.css'
+import transcriptstyles from '../../styles/components/large blocks/fullTranscriptStyles.module.css'
+import contentblock from '../../styles/components/contentblocks.module.css'
+import largeblockStyles from '../../styles/components/large blocks/createNewBlockStyles.module.css'
+
+
+function FullTranscript() {
+    return (
+        <>
+
+            <div className={`${contentblock.largeBlockContainernohover} ${flexi.flexcolumnSmolGap} ${flexi.alignstart}`}>
+                <div className={flexi.padding50vert}>
+                    <div className={`${contentblock.topContainer}`}> 
+                        <h5>Full Transcript</h5>
+                    </div>
+
+                    <div className={`${contentblock.topContainer}`}> 
+                        <ul>
+                            <li> <p style={{color: `var(--Final_White)`}}>Sentence 1</p></li>
+                            <li> <p style={{color: `var(--Final_White)`}}>Sentence 2</p></li>
+                            <li> <p style={{color: `var(--Final_White)`}}>Sentence 3</p></li>
+                            <li> <p style={{color: `var(--Final_White)`}}>Sentence 1</p></li>
+                            <li> <p style={{color: `var(--Final_White)`}}>Sentence 2</p></li>
+                            <li> <p style={{color: `var(--Final_White)`}}>Sentence 3</p></li>
+                            <li> <p style={{color: `var(--Final_White)`}}>Sentence 1</p></li>
+                            <li> <p style={{color: `var(--Final_White)`}}>Sentence 2</p></li>
+                            <li> <p style={{color: `var(--Final_White)`}}>Sentence 3</p></li>
+                            <li> <p style={{color: `var(--Final_White)`}}>Sentence 1</p></li>
+                            <li> <p style={{color: `var(--Final_White)`}}>Sentence 2</p></li>
+                            <li> <p style={{color: `var(--Final_White)`}}>Sentence 3</p></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className={`${contentblock.largeBlockContainernohover} ${flexi.flexcolumnSmolGap} ${flexi.alignstart}`}>
+                <div className={flexi.padding50vert}>
+                    <div className={`${contentblock.topContainer} ${flexi.flexrowSmolGap} ${flexi.alignSpacebetween}`}> 
+                        <h5>Full Transcript</h5>
+                        <div className={logos.small} style={{ backgroundImage: `url("/icons/Edit.png")`, zIndex: 1 }}></div>
+                    </div>
+
+                    <div className={`${contentblock.topContainer}`}> 
+                        <ul>
+                            <li>
+                                <p style={{color: `var(--Final_White)`}}>Sentence 1</p>
+                                <div className={largeblockStyles.line}></div>
+                            </li>
+                            <li>
+                                <p style={{color: `var(--Final_White)`}}>Sentence 2</p>
+                                <div className={largeblockStyles.line}></div>
+                            </li>                            
+                            <li>
+                                <p style={{color: `var(--Final_White)`}}>Sentence 3</p>
+                                <div className={largeblockStyles.line}></div>
+                            </li>
+                            <li>
+                                <p style={{color: `var(--Final_White)`}}>Sentence 3</p>
+                                <div className={largeblockStyles.line}></div>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+            
+            <div className={`${contentblock.largeBlockContainernohover} ${flexi.flexcolumnSmolGap} ${flexi.alignstart}`}>
+                <div className={flexi.padding50vert}>
+                    <div className={`${contentblock.topContainer} ${flexi.flexrowSmolGap} ${flexi.alignSpacebetween}`}> 
+                        <h5>Full Transcript</h5>
+                        <div className={`${flexi.flexrowSmolGap}`}>
+                            <div className={logos.small} style={{ backgroundImage: `url("/icons/Check.png")`, zIndex: 1 }}></div>
+                            <div className={logos.small} style={{ backgroundImage: `url("/icons/Cancellation.png")`, zIndex: 1 }}></div>
+                        </div>
+                    </div>
+
+                    <div className={`${contentblock.topContainer}`}> 
+                        <ul>
+                            <li>
+                                <input type='text' placeholder='Sentence 1'></input>
+                                <div className={largeblockStyles.line}></div>
+                            </li>
+                            <li>
+                                <input type='text' placeholder='Sentence 2'></input>
+                                <div className={largeblockStyles.line}></div>
+                            </li>                            
+                            <li>
+                                <input type='text' placeholder='Sentence 3'></input>
+                                <div className={largeblockStyles.line}></div>
+                            </li>
+                            <li>
+                                <input type='text' placeholder='Sentence 4'></input>
+                                <div className={largeblockStyles.line}></div>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default FullTranscript
