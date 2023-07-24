5b89be7343fb8e592353f6b2a064ecf59a6cc136
Shockbob17 <94674990+Shockbob17@users.noreply.github.com>
2023-07-24 10:50:37 +0800

    Finalization of consolidation

--- SatiscribeFrontEnd/components/large blocks/createNewBlocks.js
+++ SatiscribeFrontEnd/components/large blocks/createNewBlocks.js
@@ -9,30 +9,35 @@ function CreateNewBlock() {
     return (
         <>
             <div className={`${contentblock.largeBlockContainer} ${flexi.flexColumnSmolGap} ${flexi.alignCenter}`}>
-                <div className={`${flexi.flexRowSmolGap} ${flexi.alignCenter}`}>
-                    <div className={logos.big} style={{ backgroundImage: `url("/icons/plus.png")`, zIndex: 1, }}></div>
-                    <h2 style={{ color: `var(--Final_Light_Purple)` }}> New Component</h2>
+                <div className={`${flexi.innerMargin}`}>
+                    <div className={`${flexi.flexRowSmolGap} ${flexi.alignCenter}`}>
+                        <div className={logos.big} style={{ backgroundImage: `url("/icons/plus.png")`, zIndex: 1, }}></div>
+                        <h2 style={{ color: `var(--Final_Light_Purple)` }}> New Component</h2>
+                    </div>
                 </div>
             </div>
 
-            <div className={`${contentblock.largeBlockContainerNoHover} ${contentblock.contentBlockAlignment}`}>
-                <div className={`${flexi.flexColumnMediumGap}`}>
-                    <h5>Please Choose A Content Block</h5>
+            <div className={`${contentblock.largeBlockContainerNoHover}`}>
+                <div className={`${flexi.innerMargin}`}>
 
-                    <div>
-                        <p style={{ color: `var(--Final_White)` }}>Task Summariser and Assigner Block</p>
-                        <div className={largeblockStyles.line}></div>
+                    <div className={`${flexi.flexColumnMediumGap}`}>
+                        <h5>Please Choose A Content Block</h5>
+
+                        <div>
+                            <p style={{ color: `var(--Final_White)` }}>Task Summariser and Assigner Block</p>
+                            <div className={largeblockStyles.line}></div>
+                        </div>
+
+                        <div>
+                            <p style={{ color: `var(--Final_White)` }}>Full Transcript Block</p>
+                            <div className={largeblockStyles.line}></div>
+                        </div>
+
                     </div>
-
-                    <div>
-                        <p style={{ color: `var(--Final_White)` }}>Full Transcript Block</p>
-                        <div className={largeblockStyles.line}></div>
-                    </div>
-
                 </div>
             </div>
 
-            <div className={contentblock.largeBlockContainerNoHover} style={{ backgroundImage: `url(/components/thetrueoptions.png)`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <div className={`${contentblock.largeBlockContainerNoHover}`} style={{ backgroundImage: `url(/components/thetrueoptions.png)`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
                 <div className={largeblockStyles.buttonPlacement} style={{ right: `calc(50% + 16.5vh)`, bottom: `calc(50% + 5.5vh)` }}></div>
                 <div className={largeblockStyles.buttonPlacement} style={{ left: `calc(50% + 16vh)`, bottom: `calc(50% + 5.5vh)` }}></div>
                 <div className={largeblockStyles.buttonPlacement} style={{ right: `calc(50% + 16.5vh)`, top: `calc(50% + 6vh)` }}></div>
--- SatiscribeFrontEnd/components/large blocks/fullTranscript.js
+++ SatiscribeFrontEnd/components/large blocks/fullTranscript.js
@@ -11,81 +11,75 @@ function FullTranscript() {
     return (
         <>
 
-            {/* <div className={`${contentblock.largeBlockContainerNoHover} ${contentblock.contentBlockAlignment} `}>
-                <div className={`${contentblock.topContainer}`}>
+            <div className={`${contentblock.largeBlockContainerNoHover} ${contentblock.contentBlockAlignment}`}>
+                <div className={`${flexi.innerMargin}`}>
                     <h5>Full Transcript</h5>
-                </div>
 
-                <div className={`${contentblock.topContainer}`}>
                     <ul>
                         <li> <p style={{ color: `var(--Final_White)` }}>Sentence 1</p></li>
                         <li> <p style={{ color: `var(--Final_White)` }}>Sentence 2</p></li>
 
                     </ul>
                 </div>
-            </div> */}
-
-            <div className={`${contentblock.largeBlockContainerNoHover} ${contentblock.contentBlockAlignment}`}>
-                <h5>Full Transcript</h5>
-
-                <ul>
-                    <li> <p style={{ color: `var(--Final_White)` }}>Sentence 1</p></li>
-                    <li> <p style={{ color: `var(--Final_White)` }}>Sentence 2</p></li>
-
-                </ul>
             </div>
 
             <div className={`${contentblock.largeBlockContainerNoHover} ${contentblock.contentBlockAlignment}`}>
-                <div className={`${flexi.flexRowSmolGap} ${flexi.alignSpaceBetween}`}>
-                    <h5>Full Transcript</h5>
-                    <div className={logos.small} style={{ backgroundImage: `url("/icons/Edit.png")`, zIndex: 1 }}></div>
-                </div>
+                <div className={`${flexi.innerMargin}`}>
 
-                <div>
+                    <div className={`${flexi.flexRowSmolGap} ${flexi.alignSpaceBetween}`}>
+                        <h5>Full Transcript</h5>
+                        <div className={logos.small} style={{ backgroundImage: `url("/icons/Edit.png")`, zIndex: 1 }}></div>
+                    </div>
+
+                    <div>
+                        <ul>
+                            <li>
+                                <p style={{ color: `var(--Final_White)` }}>Sentence 1</p>
+                                <div className={largeblockStyles.line}></div>
+                            </li>
+                            <li>
+                                <p style={{ color: `var(--Final_White)` }}>Sentence 2</p>
+                                <div className={largeblockStyles.line}></div>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className={`${contentblock.largeBlockContainerNoHover} ${contentblock.contentBlockAlignment}`}>
+                <div className={`${flexi.innerMargin}`}>
+
+                    <div className={`${flexi.flexRowSmolGap} ${flexi.alignSpaceBetween}`}>
+                        <h5>Full Transcript</h5>
+                        <div className={`${flexi.flexRowSmolGap}`}>
+                            <div className={logos.small} style={{ backgroundImage: `url("/icons/Check.png")`, zIndex: 1 }}></div>
+                            <div className={logos.small} style={{ backgroundImage: `url("/icons/Cancellation.png")`, zIndex: 1 }}></div>
+                        </div>
+                    </div>
+
                     <ul>
                         <li>
-                            <p style={{ color: `var(--Final_White)` }}>Sentence 1</p>
+                            <input type='text' placeholder='Sentence 1'></input>
                             <div className={largeblockStyles.line}></div>
                         </li>
                         <li>
-                            <p style={{ color: `var(--Final_White)` }}>Sentence 2</p>
+                            <input type='text' placeholder='Sentence 2'></input>
+                            <div className={largeblockStyles.line}></div>
+                        </li>
+                        <li>
+                            <input type='text' placeholder='Sentence 3'></input>
+                            <div className={largeblockStyles.line}></div>
+                        </li>
+                        <li>
+                            <input type='text' placeholder='Sentence 4'></input>
                             <div className={largeblockStyles.line}></div>
                         </li>
 
                     </ul>
                 </div>
-            </div>
-
-
-
-            <div className={`${contentblock.largeBlockContainerNoHover} ${contentblock.contentBlockAlignment}`}>
-                <div className={`${flexi.flexRowSmolGap} ${flexi.alignSpaceBetween}`}>
-                    <h5>Full Transcript</h5>
-                    <div className={`${flexi.flexRowSmolGap}`}>
-                        <div className={logos.small} style={{ backgroundImage: `url("/icons/Check.png")`, zIndex: 1 }}></div>
-                        <div className={logos.small} style={{ backgroundImage: `url("/icons/Cancellation.png")`, zIndex: 1 }}></div>
-                    </div>
-                </div>
-
-                <ul>
-                    <li>
-                        <input type='text' placeholder='Sentence 1'></input>
-                        <div className={largeblockStyles.line}></div>
-                    </li>
-                    <li>
-                        <input type='text' placeholder='Sentence 2'></input>
-                        <div className={largeblockStyles.line}></div>
-                    </li>
-                    <li>
-                        <input type='text' placeholder='Sentence 3'></input>
-                        <div className={largeblockStyles.line}></div>
-                    </li>
-                    <li>
-                        <input type='text' placeholder='Sentence 4'></input>
-                        <div className={largeblockStyles.line}></div>
-                    </li>
-
-                </ul>
             </div>
         </>
     )
