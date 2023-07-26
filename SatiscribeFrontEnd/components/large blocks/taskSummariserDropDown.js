278b8d92bc54b6cf4a57bf4f08d61730ea54c797
Shockbob17 <94674990+Shockbob17@users.noreply.github.com>
2023-07-26 23:56:55 +0800

    updated images

--- SatiscribeFrontEnd/components/large blocks/taskSummariserDropDown.js
+++ SatiscribeFrontEnd/components/large blocks/taskSummariserDropDown.js
@@ -84,7 +84,7 @@ function TaskSummariserDropDown({ clickable, onClick, dataset, resetDropDown }) 
                                 onClick={() => onClick(member)}>
 
                                 <div className={`${flexi.flexRowSmolGap} ${contentblock.clickable}`}>
-                                    <div className={logos.small} style={{ backgroundImage: `url(${dataset[member] ? '/icons/Check.png' : '/icons/Cancellation.png'})`, zIndex: 1 }}></div>
+                                    <div className={logos.small} style={{ backgroundImage: `url(${dataset[member] ? '/icons/CheckboxTicked.png' : '/icons/Checkbox.png'})`, zIndex: 1 }}></div>
                                     <div className={logos.small} style={{ backgroundImage: `url("/profiles/Profile Pict (Cream).png")`, zIndex: 1 }}></div>
                                     <p style={{ color: `var(--Final_White)` }}>{member}</p>
                                 </div>
