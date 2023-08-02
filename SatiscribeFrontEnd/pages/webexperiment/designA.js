7c0531a78408c185f233f0662cdad06cd021fe7c
Shockbob17 <94674990+Shockbob17@users.noreply.github.com>
2023-08-02 22:13:38 +0800

    Finalize webexperiment

--- SatiscribeFrontEnd/pages/webexperiment/designA.js
+++ SatiscribeFrontEnd/pages/webexperiment/designA.js
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState } from 'react'
-import Button  from '@/components/buttons/button.js';
+import Button from '@/components/buttons/button.js';
 import colourstyle from '/styles/Colourtest.module.css';
 import logos from '/styles/Logos.module.css';
 import landing from '@/styles/webexperiment/Landingpage.module.css';
@@ -22,28 +22,33 @@ function DesignA() {
 
     return (
         <StyledComponentsRegistry>
-        <Nonclicknavbar/>
-        <div className={landing.headers}>
-            <Button
-                size="small"
-                logo="left"
-                logoStyle={{ backgroundImage: `url("/iconsGrey/Replace.png")`, zIndex: 1 }}
-                disabled= {true}
-            >
-                Reset All Changes
-            </Button>
-            <h4>Edit Full Transcript</h4>
-            <Button 
-                size="small"
-                logo="left"
-                fill={true}
-                logoStyle={{ backgroundImage: `url("/iconsFinalGray/Check.png")`, zIndex: 1 }} // Change this to the URL of your right logo
-                onClick={handleNavigate}
-            >
-                Finalise All Changes
-            </Button>
-        </div>
-        <VettingBlock SentenceData={SentenceData}/>
+            <Nonclicknavbar />
+            <div className={landing.headers}>
+                <Button
+                    size="small"
+                    logo="left"
+                    logoStyle={{ backgroundImage: `url("/iconsGrey/Replace.png")`, zIndex: 1 }}
+                    disabled={true}
+                >
+                    Reset All Changes
+                </Button>
+                <h4>Edit Full Transcript</h4>
+                <Button
+                    size="small"
+                    logo="left"
+                    fill={true}
+                    logoStyle={{ backgroundImage: `url("/iconsFinalGray/Check.png")`, zIndex: 1 }} // Change this to the URL of your right logo
+                    onClick={handleNavigate}
+                >
+                    Finalise All Changes
+                </Button>
+            </div>
+            <VettingBlock SentenceData={SentenceData} />
+            <div className={landing.fixme}>
+                <a href='#'>
+                    <p> Go to top </p>
+                </a>
+            </div>
         </StyledComponentsRegistry>
 
 
