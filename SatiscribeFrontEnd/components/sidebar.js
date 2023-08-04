a21f6cdb30baa6f0b284237014866cf9ed39cb09
hubertleesw <hubertlee_99@hotmail.co.uk>
2023-08-05 07:34:25 +0800

    Test Route

--- SatiscribeFrontEnd/components/sidebar.js
+++ SatiscribeFrontEnd/components/sidebar.js
@@ -1,15 +1,19 @@
+'use client'
+
 import { useState } from 'react';
 import styled from 'styled-components';
 import { AiOutlineMenu } from 'react-icons/ai';
 import logos from '/styles/Logos.module.css'
 // import { Button, ButtonWithLogo } from '../components';
 import styles from '/styles/sidebar.module.css'
+import StyledComponentsRegistry from '../components/registery.tsx'
+
 
 const SidebarStyled = styled.div`
 display: ${props => props.isOpen ? 'block' : 'none'};
 width: ${props => props.isOpen ? '15vw' : '0'};
 position: fixed;
-top: 80px;  // Adjust this value to shift the sidebar down
+top: 100px;  // Adjust this value to shift the sidebar down
 height: calc(99% - 10px);  // Adjust the height to keep the sidebar within the viewport
 background-color: #333;
 color: #fff;
@@ -38,10 +42,11 @@ const Sidebar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div>
-            <button className={logos.small} style={{ backgroundImage: `url("/icons/PurpleHamburger.png")`, backgroundColor: 'transparent', zIndex: 1, border: 'none',   marginLeft: '20px'}} onClick={() => setIsOpen(!isOpen)} />
-            <SidebarStyled isOpen={isOpen}>
-                {/* <Button size="small" onClick={() => alert('Small button clicked!')}>
+        <StyledComponentsRegistry>
+            <div>
+                <button className={logos.small} style={{ backgroundImage: `url("/icons/PurpleHamburger.png")`, backgroundColor: 'transparent', zIndex: 1, border: 'none', marginLeft: '20px' }} onClick={() => setIsOpen(!isOpen)} />
+                <SidebarStyled isOpen={isOpen}>
+                    {/* <Button size="small" onClick={() => alert('Small button clicked!')}>
                     Small Button
                 </Button>
                 <Button size="small" onClick={() => alert('Small button clicked!')}>
@@ -50,13 +55,14 @@ const Sidebar = () => {
                 <Button size="small" onClick={() => alert('Small button clicked!')}>
                     Small Button
                 </Button> */}
-                <h6 className={styles.projectcontainer}> Create New</h6>
-                <h6 className={styles.projectcontainer}> Project Name</h6>
-                <h6 className={styles.projectcontainer}> Project Name</h6>
-                <h6 className={styles.projectcontainer}> Project Name</h6>
-                <h6 className={styles.projectcontainer}> Project Name</h6>
-            </SidebarStyled>
-        </div>
+                    <h6 className={styles.projectcontainer}> Create New</h6>
+                    <h6 className={styles.projectcontainer}> Project Name</h6>
+                    <h6 className={styles.projectcontainer}> Project Name</h6>
+                    <h6 className={styles.projectcontainer}> Project Name</h6>
+                    <h6 className={styles.projectcontainer}> Project Name</h6>
+                </SidebarStyled>
+            </div>
+        </StyledComponentsRegistry>
     );
 }
 
